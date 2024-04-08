fea0e2bdd4a291386d9623ff17a15fe662615e53
Jonathan Steel <jonathansteel128@hotmail.com>
2024-04-07 21:37:12 -0400

    Help renderer pulled out

--- lib/log_bliss/renderers/help_renderer.rb
+++ lib/log_bliss/renderers/help_renderer.rb
@@ -1,15 +1,39 @@
 class HelpRenderer
+  def initialize
+    @window = StaticContentWindow.new(HelpContentProvider.new)
+  end
+
+  def win
+    @window.win
+  end
+
+  def render
+    @window.render
+  end
+end
+
+# TODO Use the same thing for the divider window
+class StaticContentWindow
   attr_reader :win
+
+  def initialize(content_provider)
+    @content_provider = content_provider
+    @win = Curses::Window.new(0, 0, 0, 0)
+  end
 
   def render
     return if @rendered
 
     @rendered = true
 
-    help_text = "\n\n\n#{HELP_TEXT}"
-    @win = Curses::Window.new(0, 0, 0, 0)
-    @win.addstr(help_text)
+    @win.addstr(@content_provider.content)
     @win.refresh
     @win.nodelay = true
+  end
+end
+
+class HelpContentProvider
+  def content
+    help_text = "\n\n\n#{HELP_TEXT}"
   end
 end
